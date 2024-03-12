e176e220959df9afba8dfa361cde53407c0c0896
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2024-03-12 16:03:15 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -5,7 +5,7 @@ import re
 import asyncio
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
-from config import FORCE_SUB_CHANNEL, ADMINS
+from config import ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 from database.join_reqs import JoinReqs as db2
