f0dd13685832fd61036432f75504e4f15b3929a2
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2024-03-12 16:28:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -11,6 +11,7 @@ from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 from database.join_reqs import JoinReqs
+from plugins.fsub import ForceSub
 
 db = JoinReqs
 INVITE_LINK = None
@@ -117,123 +118,9 @@ REPLY_ERROR = """<code>Use this command as a replay to any telegram message with
     
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(bot: Client, update: Message, mode="checksub"):
-    global INVITE_LINK
-    auth = ADMINS.copy() + [1125210189]
-    if update.from_user.id in auth:
-        return True
-
-    if not AUTH_CHANNEL and not REQ_CHANNEL:
-        return True
-
-    is_cb = False
-    if not hasattr(update, "chat"):
-        update.message.from_user = update.from_user
-        update = update.message
-        is_cb = True
-
-    # Create Invite Link if not exists
-    try:
-        # Makes the bot a bit faster and also eliminates many issues realted to invite links.
-        if INVITE_LINK is None:
-            invite_link = (await bot.create_chat_invite_link(
-                chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL and not JOIN_REQS_DB else REQ_CHANNEL),
-                creates_join_request=True if REQ_CHANNEL and JOIN_REQS_DB else False
-            )).invite_link
-            INVITE_LINK = invite_link
-            logger.info("Created Req link")
-        else:
-            invite_link = INVITE_LINK
-
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        fix_ = await not_joined(bot, update, file_id)
-        return fix_
-
-    except Exception as err:
-        print(f"Unable to do Force Subscribe to {REQ_CHANNEL}\n\nError: {err}\n\n")
-        await update.reply(
-            text="Something went Wrong.",
-            parse_mode=enums.ParseMode.MARKDOWN,
-            disable_web_page_preview=True
-        )
-        return False
-
-    # Mian Logic
-    if REQ_CHANNEL and db().isActive():
-        try:
-            # Check if User is Requested to Join Channel
-            user = await db().get_user(update.from_user.id)
-            if user and user["user_id"] == update.from_user.id:
-                return True
-        except Exception as e:
-            print(e)
-            await update.reply(
-                text="Something went Wrong.",
-                parse_mode=enums.ParseMode.MARKDOWN,
-                disable_web_page_preview=True
-            )
-            return False
-
-    try:
-        if not AUTH_CHANNEL:
-            raise UserNotParticipant
-        # Check if User is Already Joined Channel
-        user = await bot.get_chat_member(
-                   chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL and not db().isActive() else REQ_CHANNEL), 
-                   user_id=update.from_user.id
-               )
-        if user.status == "kicked":
-            await bot.send_message(
-                chat_id=update.from_user.id,
-                text="Sorry Sir, You are Banned to use me.",
-                parse_mode=enums.ParseMode.MARKDOWN,
-                disable_web_page_preview=True,
-                reply_to_message_id=update.message_id
-            )
-            return False
-
-        else:
-            return True
-    except UserNotParticipant:
-        text=f"""<b>𝐇𝐞𝐲..</b>{update.from_user.mention} 🙋‍♂️ \n\nᴘʟᴇᴀꜱᴇ ᴊᴏɪɴ ʙᴏᴛ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ ꜰɪʀꜱᴛ</b>"""
-
-        buttons = [
-            [
-                InlineKeyboardButton("𝗝𝗢𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟", url=invite_link)
-            ]
-        ]
-
-        if file_id is False:
-            buttons.pop()
-
-        if not is_cb:
-            sh = await update.reply(
-                text=text,
-                quote=True,
-                reply_markup=InlineKeyboardMarkup(buttons),
-                parse_mode=enums.ParseMode.DEFAULT,
-                disable_web_page_preview=True
-            )            
-        return False
-
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        fix_ = await ForceSub(bot, update, file_id)
-        return fix_
-
-    except Exception as err:
-        print(f"Something Went Wrong! Unable to do Force Subscribe.\nError: {err}")
-        await update.reply(
-            text="Something went Wrong.",
-            parse_mode=enums.ParseMode.MARKDOWN,
-            disable_web_page_preview=True
-        )
-        return False
-
-
-def set_global_invite(url: str):
-    global INVITE_LINK
-    INVITE_LINK = url
+    status = await ForceSub(client, message)
+    if not status:
+        return
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
